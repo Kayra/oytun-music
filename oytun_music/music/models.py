dbb429848d7510c0116763e581ab8f2d63ea6408
Kayra <KayraAlat@gmail.com>
2018-11-22 13:36:29 +0000

    Added performance model

--- oytun_music/music/models.py
+++ oytun_music/music/models.py
@@ -13,7 +13,18 @@ class Site(models.Model):
 class Song(models.Model):
 
     title = models.CharField(max_length=200)
-    link = models.CharField(max_length=200)
+    external_link = models.CharField(max_length=200)
 
     def __str__(self):
         return self.title
+
+
+class Performance(models.Model):
+
+    venue_name = models.CharField(max_length=200)
+    venue_address = models.CharField(max_length=400)
+    start_time = models.DateTimeField()
+    external_link = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.venue_name
