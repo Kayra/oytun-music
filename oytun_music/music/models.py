f875f2af51a2f0ef7cf7c0b994eac5105074b033
Kayra <KayraAlat@gmail.com>
2018-11-26 20:47:33 +0000

    Updated models

--- oytun_music/music/models.py
+++ oytun_music/music/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Site(models.Model):
 
     title = models.CharField(max_length=200)
-    about_me = models.TextField()
-    number_of_songs_to_list = models.IntegerField()
-    number_of_performances_to_list = models.IntegerField()
+    about_me = models.TextField(blank=True)
+    number_of_songs_to_list = models.IntegerField(default=5)
+    number_of_performances_to_list = models.IntegerField(default=5)
 
     def __str__(self):
         return self.title
@@ -16,7 +16,7 @@ class Song(models.Model):
 
     title = models.CharField(max_length=200)
     external_link = models.CharField(max_length=200)
-    order = models.IntegerField()
+    order = models.IntegerField(blank=True)
 
     def __str__(self):
         return self.title
@@ -26,9 +26,9 @@ class Performance(models.Model):
 
     venue_name = models.CharField(max_length=200)
     venue_address = models.CharField(max_length=400)
-    start_time = models.DateTimeField()
-    external_link = models.CharField(max_length=200)
-    order = models.IntegerField()
+    start_time = models.DateTimeField(blank=True)
+    external_link = models.CharField(max_length=200, blank=True)
+    order = models.IntegerField(blank=True)
 
     def __str__(self):
         return self.venue_name
