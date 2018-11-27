80f28ff81782b695afd0f46c64f5c79c90d4a42b
Kayra <KayraAlat@gmail.com>
2018-11-27 21:56:59 +0000

    Added initial index view

--- oytun_music/music/views.py
+++ oytun_music/music/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 
-# from .models import Site, Song, Performance
-#
-# def index(request):
-#
-#     site_details = Site.objects.first()
-#     last_five_songs = Song.objects
+from .models import Site, Song, Performance
+
+
+def index(request):
+
+    site_details = Site.objects.first()
+    last_five_songs = Song.objects.order_by('order')[:site_details.number_of_songs_to_list]
+    last_five_performances = Performance.objects.order_by('order')[:site_details.number_of_performances_to_list]
+
+    index_template = 'music/index.html'
+    template_context = {
+        'site_title': site_details.title,
+        'about_me': site_details.about_me,
+        'last_five_songs': last_five_songs,
+        'last_five_performances': last_five_performances
+    }
+
+    return render(request, index_template, template_context)
