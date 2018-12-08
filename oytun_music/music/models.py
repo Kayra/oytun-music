172419400a8c4f6cb7930099cb05af0b1c0dd10c
Kayra <KayraAlat@gmail.com>
2018-12-08 15:38:22 +0000

    Updated column names

--- oytun_music/music/models.py
+++ oytun_music/music/models.py
@@ -5,8 +5,8 @@ class Site(models.Model):
 
     title = models.CharField(max_length=200)
     about_me = models.TextField(null=True, blank=True)
-    number_of_songs_to_list = models.IntegerField(default=5)
-    number_of_performances_to_list = models.IntegerField(default=5)
+    number_of_songs_to_display = models.IntegerField(default=5)
+    number_of_performances_to_display = models.IntegerField(default=5)
 
     def __str__(self):
         return self.title
@@ -16,7 +16,7 @@ class Song(models.Model):
 
     title = models.CharField(max_length=200)
     external_link = models.CharField(max_length=200)
-    order = models.IntegerField(null=True, blank=True)
+    display_order = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.title
@@ -28,7 +28,7 @@ class Performance(models.Model):
     venue_address = models.CharField(max_length=400)
     start_time = models.DateTimeField(null=True, blank=True)
     external_link = models.CharField(max_length=200, null=True, blank=True)
-    order = models.IntegerField(null=True, blank=True)
+    display_order = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.venue_name
