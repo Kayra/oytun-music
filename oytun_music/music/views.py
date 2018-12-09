4bff8bb24c961e95380e497833a595b3bd747871
Kayra <KayraAlat@gmail.com>
2018-12-09 16:07:54 +0000

    Fixed view to reflect new models

--- oytun_music/music/views.py
+++ oytun_music/music/views.py
@@ -6,8 +6,8 @@ from .models import Site, Song, Performance
 def index(request):
 
     site_details = Site.objects.first()
-    songs_to_display = Song.objects.order_by('order')[:site_details.number_of_songs_to_list]
-    performances_to_display = Performance.objects.order_by('order')[:site_details.number_of_performances_to_list]
+    songs_to_display = Song.objects.order_by('display_order')[:site_details.number_of_songs_to_display]
+    performances_to_display = Performance.objects.order_by('display_order')[:site_details.number_of_performances_to_display]
 
     index_template = 'music/index.html'
     template_context = {
