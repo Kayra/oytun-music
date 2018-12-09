f092f0c964db98d567a859737c9c25db7e587dde
Kayra <KayraAlat@gmail.com>
2018-12-09 16:02:41 +0000

    Updated seed db bash script to run from project root

--- scripts/seed_db.sh
+++ scripts/seed_db.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export PGPASSWORD=development_password_only
-psql -h localhost -d oytun_music -U oytun_music -p 5432 -a -w -f seed_db.sql
+psql -h localhost -d oytun_music -U oytun_music -p 5432 -a -w -f scripts/seed_db.sql
