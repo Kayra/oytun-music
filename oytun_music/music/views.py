f71dedd5226c605dc055a174e11859c469d847f4
Kayra <KayraAlat@gmail.com>
2018-12-14 11:19:53 +0000

    Added dynamic maps url

--- oytun_music/music/views.py
+++ oytun_music/music/views.py
@@ -1,3 +1,5 @@
+from urllib.parse import quote
+
 from django.shortcuts import render
 
 from .models import Site, Song, Performance
@@ -8,6 +10,11 @@ def index(request):
     site_details = Site.objects.first()
     songs_to_display = Song.objects.order_by('display_order')[:site_details.number_of_songs_to_display]
     performances_to_display = Performance.objects.order_by('display_order')[:site_details.number_of_performances_to_display]
+
+    for performance in performances_to_display:
+        formatted_address = '+'.join(performance.venue_address.split())
+        full_google_maps_url = "https://www.google.com/maps/place/" + formatted_address
+        performance.google_maps_url = full_google_maps_url
 
     index_template = 'music/index.html'
     template_context = {
