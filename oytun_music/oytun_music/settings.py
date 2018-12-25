f64191e01f4905458a5afe1c3087cb6456419e46
Kayra <KayraAlat@gmail.com>
2018-12-25 18:09:14 +0000

    Added aws db settings

--- oytun_music/oytun_music/settings.py
+++ oytun_music/oytun_music/settings.py
@@ -74,16 +74,29 @@ WSGI_APPLICATION = 'oytun_music.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'oytun_music',
-        'USER': 'oytun_music',
-        'PASSWORD': 'oytun_music',
-        'HOST': 'localhost',
-        'PORT': '5432'
+
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'oytun_music',
+            'USER': 'oytun_music',
+            'PASSWORD': 'oytun_music',
+            'HOST': 'localhost',
+            'PORT': '5432'
+        }
+    }
 
 
 # Password validation
