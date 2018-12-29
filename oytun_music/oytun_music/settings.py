4daa6ec54d2b8db09fad148eff60e7467a915efb
Kayra <KayraAlat@gmail.com>
2018-12-29 12:56:15 +0000

    Added static file handling for aws

--- oytun_music/oytun_music/settings.py
+++ oytun_music/oytun_music/settings.py
@@ -134,6 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads/')
 MEDIA_URL = os.path.join(BASE_DIR, 'uploads/')
