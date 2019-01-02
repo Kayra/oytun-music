9f93589a941d71fbf7a85ec33af2c064dff0e536
Kayra <KayraAlat@gmail.com>
2019-01-02 12:57:24 +0000

    Updated settings for s3

--- oytun_music/oytun_music/settings.py
+++ oytun_music/oytun_music/settings.py
@@ -166,10 +166,12 @@ if not settings.DEBUG:
     # you run `collectstatic`).
 
     STATICFILES_LOCATION = 'static'
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+
+    MEDIAFILES_LOCATION = 'media'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'music/static'),
     ]
     STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-    DEFAULT_FILE_STORAGE = 'mysite.storage_backends.MediaStorage'
